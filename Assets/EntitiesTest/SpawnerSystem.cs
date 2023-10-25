6d23ed5171de7044d265b2dd73828fa13f1e545f
DESKTOP-4S2DC32\A <626150455@qq.com>
2023-10-25 18:08:17 +0800

    dots官方示例运行

--- Assets/EntitiesTest/SpawnerSystem.cs
+++ Assets/EntitiesTest/SpawnerSystem.cs
@@ -2,13 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Burst;
 using Unity.Entities;
+using Unity.Mathematics;
 using Unity.Transforms;
 using UnityEngine;
 using USDT.Utils;
 
 [BurstCompile]
 public partial struct SpawnerSystem : ISystem {
-    public void OnCreate(ref SystemState state) { }
+    Unity.Mathematics.Random _random;
+    public void OnCreate(ref SystemState state) {
+        _random = new Unity.Mathematics.Random();
+        _random.InitState();
+    }
 
     public void OnDestroy(ref SystemState state) { }
 
@@ -18,10 +23,13 @@ public partial struct SpawnerSystem : ISystem {
             if(sdata.ValueRO.nextSpawnTime < SystemAPI.Time.ElapsedTime) {
                 // 创建一个新的实体
                 Entity newE = state.EntityManager.Instantiate(sdata.ValueRO.prefab);
-                // 添加LocalTransform组件
-                state.EntityManager.SetComponentData(newE, LocalTransform.FromPosition(sdata.ValueRO.spawnPos));
 
-                //state.EntityManager.SetComponentData(newE, RenderMesh)
+                // 添加LocalTransform组件
+                var startV = sdata.ValueRO.spawnPos;
+                startV.y += _random.NextFloat(1, 11);
+                var rq = quaternion.RotateX(_random.NextInt(0, 360));
+                var newPos = math.mul(rq, startV);
+                state.EntityManager.SetComponentData(newE, LocalTransform.FromPosition(newPos));
 
                 // 制定下次实例时间
                 sdata.ValueRW.nextSpawnTime = (float)SystemAPI.Time.ElapsedTime + sdata.ValueRO.spawnRate;
