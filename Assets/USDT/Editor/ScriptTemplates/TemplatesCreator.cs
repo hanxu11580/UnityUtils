70b988d89d047331d685c6a9aa5623f7e2dae627
DESKTOP-4S2DC32\A <626150455@qq.com>
2023-12-07 12:46:18 +0800

    dots 自动生成模版脚本: authoring componentdata system

---
+++ Assets/USDT/Editor/ScriptTemplates/TemplatesCreator.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace USDT.CustomEditor.ScriptTemplates {
+    public static class TemplatesCreator
+    {
+        // ecs
+        static readonly string ComponentTemplate = "c6ae1b97fcfea5f44b65dd7c16df1747";
+        static readonly string AuthoringTemplate = "f4abf72bf1e3d8e4bb444cbed495b9f3";
+        static readonly string SystemTemplate = "f3ae3995ab21bf54cae8cc6e6f99f8a3";
+
+        [MenuItem("Assets/Create/ScriptTemplates/ECS/Component")]
+        private static void NewComponent()
+        {
+            string templatePath = AssetDatabase.GUIDToAssetPath(ComponentTemplate);
+            ProjectWindowUtil.CreateScriptAssetFromTemplateFile(templatePath, "NewComponent.cs");
+        }
+
+        [MenuItem("Assets/Create/ScriptTemplates/ECS/Authoring")]
+        private static void NewAuthoring()
+        {
+            string templatePath = AssetDatabase.GUIDToAssetPath(AuthoringTemplate);
+            ProjectWindowUtil.CreateScriptAssetFromTemplateFile(templatePath, "NewAuthoring.cs");
+        }
+
+        [MenuItem("Assets/Create/ScriptTemplates/ECS/System")]
+        private static void NewSystem()
+        {
+            string templatePath = AssetDatabase.GUIDToAssetPath(SystemTemplate);
+            ProjectWindowUtil.CreateScriptAssetFromTemplateFile(templatePath, "NewSystem.cs");
+        }
+        //
+
+        // 
+    }
+}
